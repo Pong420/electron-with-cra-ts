404994558bcdd6127acaa47ed2e4a9f6f74eadc4
Pong <samfunghp@gmail.com>
2019-03-31 16:41:01 +0800

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomePage } from './page/HomePage';
+import { PATHS } from './constants';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={HomePage} />
+      <Route exact path={PATHS.HOME} component={HomePage} />
     </Switch>
   </Router>
 );
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+declare module '*.png';
+declare module '*.svg' {
+  export const ReactComponent: SvgrComponent;
+}
