2ebcd073b126b9577e18859da323a1e084dec996
Pong <samfunghp@gmail.com>
2019-11-22 16:04:55 +0800

    make it better

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="pong-react-scripts" />
 
+declare module '*.scss';
+
 declare interface Window {
   __REDUX_DEVTOOLS_EXTENSION__: any;
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
